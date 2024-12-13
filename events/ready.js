737725856fe6562c7345c11dc0b26151297ff383
Farahat <67980665+iseeface@users.noreply.github.com>
2024-12-13 14:22:44 +0700

    delete load adzanHandler.js in ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,11 @@
 const { scheduleQOTD } = require('../handlers/quoteHandler');
-const { scheduleAdzanReminder } = require('../handlers/adzanHandler');
 
 module.exports = {
     once: true,
     async execute(client) {
         console.log(`Bot siap! Login sebagai ${client.user.tag}`);
 
-        // Jadwalkan QOTD dan pengingat Adzan
+        // Jadwalkan QOTD
         scheduleQOTD(client); // pastikan fungsi ini benar-benar membutuhkan client
-        scheduleAdzanReminder(client); // pastikan fungsi ini benar-benar membutuhkan client
     },
 };
